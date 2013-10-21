7cb7710e8a5ffe99d8351f6876a705a8cff66aaa
Ondrej Hejda <ondrejh.ck@gmail.com>
2013-10-21 20:49:20 +0200

    configuration moved into sep. file

---
+++ gui/ebio_config.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python3
+
+''' serial port setup '''
+portname = 'COM7'
+
+''' MySQL server setup:
+Assuming database 'ebio' with table 'temp' and 'heating' is present on the server.
+Table 'temp' should look like:
++------+-------+
+| id   | value |
++------+-------+
+|    1 | 22.5C |
+|    2 | 22.4C |
+|    3 | ---   |
+|    4 | ---   |
+|    5 | ---   |
++------+-------+
+where 'id' is type int and 'value' is type varchar.
+Table 'heating' should look like:
++---------+--------+
+| request | status |
++---------+--------+
+| OFF     | OFF    |
++---------+--------+
+Settings:'''
+#db_host = '192.168.113.80'
+db_host = 'localhost'
+db_user = 'root'
+#db_pass = '1234'
+db_pass = ''
+db_name = 'ebio'
+db_temp_table = 'temp'
+db_heating_table = 'heating'
