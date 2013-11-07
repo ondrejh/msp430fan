44be386facb91c9e50de13d64c4421cec1d27390
ondrej <ondrejh.ck@email.cz>
2013-11-07 15:00:08 +0100

    wgi: separating t1 value

--- www/status.php
+++ www/status.php
@@ -14,6 +14,7 @@
 			echo "Failed to connect to MySQL: ".mysqli_connect_error();
 		}
 
+		$t1 = "---";
 		for ($id=1;$id<6;$id++)
 		{
 			$sql = "SELECT value FROM temp WHERE id=$id";
@@ -23,7 +24,12 @@
 			{
 				// get field information
 				$fielddata=mysqli_fetch_row($result);
-				printf("Temp.$id: %s<br>\n",$fielddata[0]);
+				if ($id!=5)
+					printf("Temperature $id: %s<br>\n",$fielddata[0]);
+				else
+					printf("MCU temperature: %s<br>\n",$fielddata[0]);
+					
+				if ($id==1) $t1=$fielddata[0];
 
 				// free result set
 				mysqli_free_result($result);
@@ -36,13 +42,16 @@
 			// get data
 			$fielddata=mysqli_fetch_row($result);
 			printf("Heating status: %s<br>\n",$fielddata[1]);
+			printf("Fuse switch: %s<br>\n",$fielddata[2]);
 
 			// free result set
 			mysqli_free_result($result);
 		}
+		
+		printf("<div id=\"t1\">%s</div>\n",$t1);
 
 		mysqli_close($con);
-		echo "Das ende<br>\n";
+		echo "<br>\n";
 	?>
 
 </p>       
