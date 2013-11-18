99a33ae6e08efffa9a4e1b02ff5bdab2bdfd5600
ondrej <ondrejh.ck@email.cz>
2013-11-18 15:28:18 +0100

    tidy up

--- www/chart.php
+++ www/chart.php
@@ -4,10 +4,9 @@
 	echo "<p>\n";
 	echo "	<META HTTP-EQUIV=\"refresh\" CONTENT=\"60\">\n";
 
-	//echo exec('whoami');
 	echo exec('cd script; ./plot.sh');
 	
-	echo "  <img src=\"script/data.png\" alt=\"chart\">\n";
+	echo "  <img class=\"center\" src=\"script/data.png\" alt=\"chart\">\n";
 	
 	echo "</p>\n";
 	echo "<p>&nbsp;</p>\n";
